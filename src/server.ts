4c426733342805053fd6fa0985107c1e46818568
RakibulHasan2 <rakibulhasan99445@gmail.com>
2023-10-21 19:22:57 +0600

    local host is running on 5000

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,16 @@ import mongoose from 'mongoose';
 import app from './app';
 import config from './config/index';
 
+
+
 async function main() {
     try {
         await mongoose.connect(config.database_url as string);
-        console.log("database is connected");
+        console.log("database is connected", typeof(config.port));
 
 
-        app.listen(config.port, () => {
-            console.log(`Example app listening on port ${config.port}`)
+        app.listen(parseInt(`${config.port}`), () => {
+            console.log(`gadget sailor listening on port ${config.port}`)
           })
 
     } catch (err) {
@@ -17,4 +19,4 @@ async function main() {
     }
 }
 
-main()
+main();
