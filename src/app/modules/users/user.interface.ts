a3abc451a56e2a636e258ca87c7031c6d175feb4
RakibulHasan2 <rakibulhasan99445@gmail.com>
2023-10-23 20:59:58 +0600

    [user controller, model, interface and service] created

---
+++ src/app/modules/users/user.interface.ts
@@ -0,0 +1,17 @@
+import { Model } from 'mongoose';
+
+export type IUserName = {
+  firstName: string;
+  lastName: string;
+};
+
+export type IUsers = {
+  name: IUserName;
+  email: string;
+  password: string;
+  phoneNumber: number;
+};
+
+export type UserModel = Model<IUsers, Record<string, unknown>>;
+
+
