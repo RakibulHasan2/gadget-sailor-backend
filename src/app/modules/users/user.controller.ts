d6304daf9c7b9e0af71a6ca9caa64304b3a7d9fd
RakibulHasan2 <rakibulhasan99445@gmail.com>
2023-10-23 21:17:28 +0600

    [catchAsync] file code done

--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -1,1 +1,20 @@
-    
+
+
+
+const createUsers = catchAsync(async (req: Request, res: Response) => {
+    const { ...usersData } = req.body;
+    const result = await UsersService.createUsers(usersData);
+  
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'user is Created successfully',
+      data: result,
+    });
+  });
+
+
+  export const usersController = {
+    createUsers
+  };
+  
