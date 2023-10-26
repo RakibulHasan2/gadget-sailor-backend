1f42b9969db661b5a8916209daf04f70109941e7
omarfarukee <umarfaruque1920@gmail.com>
2023-10-26 14:39:18 +0600

    product-created-successfully-sone

--- src/app/modules/Product/Product.service.ts
+++ src/app/modules/Product/Product.service.ts
@@ -1,7 +1,18 @@
+import config from "../../../config";
 import { IProducts } from "./Product.interface";
 import { Products } from "./Product.model";
 
 const createProduct = async (payload: IProducts): Promise<IProducts | null> => {
+    const min = 100000;
+    const max = 999999;
+    if (!payload.status) {
+        payload.status = config.default_status as string;
+    }
+    if (!payload.product_code) {
+
+        const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+        payload.product_code = randomNumber
+    }
     const result = await Products.create(payload);
     return result;
 };
