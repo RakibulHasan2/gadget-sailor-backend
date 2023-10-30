02a16c3122a4ec91a7b5757b5c53923a827f7fa4
marziamostafa <marziamostafa@gmail.com>
2023-10-30 12:21:50 +0600

    all products retrieved

--- src/app/modules/Product/products.controller.ts
+++ src/app/modules/Product/products.controller.ts
@@ -3,22 +3,40 @@ import catchAsync from "../../../shared/catchAsync";
 import { ProductsService } from "./products.service";
 import sendResponse from "../../../shared/sendResponse";
 import httpStatus from "http-status";
+import { IProducts } from "./products.interface";
 
 
- // Controller function to create all Products
- const createProducts = catchAsync(async (req: Request, res: Response) => {
-    const { ...ProductsData } = req.body;
-    const result = await ProductsService.createProducts(ProductsData);
-  
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Products is Created successfully',
-      data: result,
-    });
+// Controller function to create all Products
+const createProducts = catchAsync(async (req: Request, res: Response) => {
+  const { ...ProductsData } = req.body;
+  const result = await ProductsService.createProducts(ProductsData);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Products is Created successfully',
+    data: result,
   });
+});
 
-  
-  export const productsController = {
-    createProducts
-  }
+
+//  getAllProducts function to retrieve all products
+const getAllProducts = async (req: Request, res: Response) => {
+
+
+  const result = await ProductsService.getAllproducts();
+
+
+  sendResponse<IProducts[]>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'products retrieved successfully',
+    data: result,
+  });
+};
+
+
+export const productsController = {
+  createProducts,
+  getAllProducts
+}
--- src/app/modules/Product/products.route.ts
+++ src/app/modules/Product/products.route.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { productsController } from './products.controller';
 
 const router = express.Router();
-router.post('/add-products',productsController.createProducts);
+router.post('/add-products', productsController.createProducts);
+router.get('/get-allProducts', productsController.getAllProducts);
 
 export const productsRoute = router;
