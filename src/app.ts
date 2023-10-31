805688f5a245a49a532b494e9eb2e1eed1b5ffb3
omarfarukee <umarfaruque1920@gmail.com>
2023-10-31 12:16:50 +0600

    validation done

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,59 @@
-import express, { Application,Request,Response} from 'express';
+// import express, { Application,Request,Response} from 'express';
+// import cors from 'cors';
+// import router from './app/routes/route.index';
+// const app : Application = express();
+
+
+// //using cors
+// app.use(cors());
+
+// //parser
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
+
+// //router
+// app.use('/api/v1', router);
+
+
+// //testing..
+// app.get('/', (req : Request, res: Response) => {
+//   res.send('working successfully');
+// })
+
+
+// export default app;
+
 import cors from 'cors';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import router from './app/routes/route.index';
-const app : Application = express();
+import globalErrorHandler from './app/middleware/globalErrorHandler';
+import httpStatus from 'http-status';
 
 
-//using cors
+const app: Application = express();
+
 app.use(cors());
-
-//parser
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-//router
 app.use('/api/v1', router);
 
-
-//testing..
-app.get('/', (req : Request, res: Response) => {
-  res.send('working successfully');
-})
-
+app.use(globalErrorHandler);
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'not found ',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'api not found ....',
+      },
+    ],
+  });
+  next();
+});
 
 export default app;
+
 
 
