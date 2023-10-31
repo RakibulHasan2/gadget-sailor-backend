8f0ae8dfa062cb20cb3234491da3ff93dd84ea9b
RakibulHasan2 <rakibulhasan99445@gmail.com>
2023-10-31 19:05:41 +0600

    globalerrohander code changed

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -10,6 +10,7 @@ import config from "../../config";
 
 
 const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
+
   let statusCode = 500;
   let message = 'something went wrong';
   let errorMessages: IGlobalGenericError[] = [];
@@ -33,26 +34,35 @@ const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
     statusCode = error?.statusCode;
     errorMessages = error?.message
       ? [
-          {
-            path: '',
-            message: error?.message,
-          },
-        ]
+        {
+          path: '',
+          message: error?.message,
+        },
+      ]
       : [];
   } else if (error instanceof Error) {
     message = error?.message;
     errorMessages = error?.message
       ? [
-          {
-            path: '',
-            message: error?.message,
-          },
-        ]
+        {
+          path: '',
+          message: error?.message,
+        },
+      ]
       : [];
   }
+
+  const propertyToExtract: any = (property : string) => {
+      const { [property]: extractedValue } = error.keyValue
+      return extractedValue
+  }
+
+  const errorM = propertyToExtract(Object.keys(error.keyValue))
+
+  console.log(errorM)
   res.status(statusCode).json({
     success: false,
-    message,
+    message: `${errorM} is already used. give a new one`,
     errorMessages,
     stack: config.env !== 'production' ? error?.stack : undefined,
   });
