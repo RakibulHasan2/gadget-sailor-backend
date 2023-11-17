b24bf8437e564bd1e022b59a8d180424410e1ad3
marziamostafa <marziamostafa@gmail.com>
2023-11-17 19:48:47 +0600

    cart ccontroller added

---
+++ src/app/modules/cart/cart.controller.ts
@@ -0,0 +1,18 @@
+import httpStatus from "http-status";
+import catchAsync from "../../../shared/catchAsync";
+import sendResponse from "../../../shared/sendResponse";
+import { cartService } from "./cart.service";
+import { Request, Response } from "express";
+
+// create cart
+const create_Cart = catchAsync(async (req: Request, res: Response) => {
+    const { ...cartData } = req.body;
+    const result = await cartService.createCart(cartData);
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Added to cart successfully',
+        data: result,
+    });
+})
