ef005461c902201492d815ca024f84927d029d24
marziamostafa <marziamostafa@gmail.com>
2023-11-17 17:57:02 +0600

    cart inteface added

---
+++ src/app/modules/cart/cart.interface.ts
@@ -0,0 +1,12 @@
+import { Model } from "mongoose";
+
+export type ICart = {
+    product_name: string;
+    image: string[];
+    unit_price: number;
+    total_price: number;
+    quantity: number;
+    model: string;
+};
+
+export type IcartModel = Model<ICart, Record<string, unknown>>;
