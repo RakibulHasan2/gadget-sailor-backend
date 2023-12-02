cfad9251b0d29078f620ad6519c2f4c2eaaa4379
marziamostafa <marziamostafa@gmail.com>
2023-12-03 01:36:56 +0600

    modified

--- src/app/modules/Product/products.interface.ts
+++ src/app/modules/Product/products.interface.ts
@@ -3,21 +3,22 @@ import { Model } from 'mongoose';
 export type IProducts = {
     category_name: string;
     sub_category_name?: string;
-    brand_name:string;
+    brand_name: string;
     product_name: string;
     image: string[];
     model: string;
     description: string;
     price: number;
     product_code: number
-    status:string;
+    status: string;
     reviews: string[];
     warranty: string;
     others_info: object;
+    I_id: string;
 };
 
 
-export type  ProductsModel = Model<IProducts, Record<string, unknown>>;
+export type ProductsModel = Model<IProducts, Record<string, unknown>>;
 
 
 
