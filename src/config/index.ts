58795805a3fe00b64f5db8f1b6498ce586c18eb2
RakibulHasan2 <rakibulhasan99445@gmail.com>
2023-12-04 16:10:41 +0600

    vercel.json deleted

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,7 +5,6 @@ import path from 'path';
 dotenv.config({path: path.join(process.cwd(),'.env')});
 
 export default {
-    // env: process.env.NODE_ENV,
     port: process.env.PORT,
     database_url: process.env.DATABASE_URL,
     default_USER_pass: process.env.DEFAULT_USER_PASS,
